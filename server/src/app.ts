be728c940478862f4d2cc9d42dbb0eb6d2374274
saadobaid40 <saadobaid123@gmail.com>
2026-08-12 06:59:22 +0300

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -47,10 +47,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // ربط الواجهة المبنية من مجلد client
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, '../../dist/public')));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../dist/public/index.html'));
 });
 app.use(express.urlencoded({ extended: true }));
 
